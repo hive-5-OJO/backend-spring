cd95be74a59efe956de45160e5808760a123b857
0andrich <rlrpdj@gmail.com>
2026-02-12 09:19:52 +0900

    init

---
+++ src/main/java/org/backend/OjoApplication.java
@@ -0,0 +1,13 @@
+package org.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/backend/OjoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
