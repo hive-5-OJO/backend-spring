a2656e8720f60a26c394bfdb229f2ad3ad22a4cb
bakyunsu00 <zgustn0@gmail.com>
2026-02-23 11:28:57 +0900

    feat: 상담 키워드 검색 레포지토리 추가 - document 추가 - response dto 추가

--- src/main/java/org/backend/domain/advice/document/AdviceDocument.java
+++ src/main/java/org/backend/domain/advice/document/AdviceDocument.java
@@ -1,12 +1,16 @@
 package org.backend.domain.advice.document;
 
 
+import java.time.LocalDateTime;
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+@Getter
 @Builder
 @Document(indexName = "advices")
 public class AdviceDocument {
@@ -14,9 +18,21 @@ public class AdviceDocument {
     @Id
     private Long id;
 
+    @Field(type = FieldType.Long)
+    private Long memberId;
+
+    @Field(type = FieldType.Keyword)
+    private String adminName;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
     @Field(type = FieldType.Text, analyzer = "nori")
     private String adviceContent;
 
-    @Field(type = FieldType.Long)
-    private Long memberId;
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime createdAt;
+
+
+
 }
--- src/main/java/org/backend/domain/advice/repository/AdviceSearchRepository.java
+++ src/main/java/org/backend/domain/advice/repository/AdviceSearchRepository.java
@@ -2,9 +2,11 @@ package org.backend.domain.advice.repository;
 
 import java.util.List;
 import org.backend.domain.advice.document.AdviceDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface AdviceSearchRepository extends ElasticsearchRepository<AdviceDocument, Long> {
-    List<AdviceDocument> findByAdviceContentContaining(String keyword);
+    Page<AdviceDocument> findByAdviceContentContaining(String keyword, Pageable pageable);
 
 }
