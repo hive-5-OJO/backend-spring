0a27b8aff4c3f4df3eaaf90b6b299f30e3323f15
Heyjinu <norang2810@naver.com>
2026-02-25 11:38:56 +0900

    fix: PR-60 verification_codes_id PK 컬럼명 매핑 수정

--- src/main/java/org/backend/domain/auth/entity/VerificationCode.java
+++ src/main/java/org/backend/domain/auth/entity/VerificationCode.java
@@ -21,6 +21,7 @@ public class VerificationCode {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "verification_codes_id")
     private Long id;
 
     @Column(nullable = false)
