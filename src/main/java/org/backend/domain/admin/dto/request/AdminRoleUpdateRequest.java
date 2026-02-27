a9246b7d7e27aae7e4fafacf9a9537b707e89e28
Heyjinu <norang2810@naver.com>
2026-02-27 15:31:20 +0900

    feat:관리자 권한 변경 API 및 구글 첫 로그인 GUEST 적용 및 dto 폴더구조 수정

---
+++ src/main/java/org/backend/domain/admin/dto/request/AdminRoleUpdateRequest.java
@@ -0,0 +1,6 @@
+package org.backend.domain.admin.dto;
+
+import org.backend.domain.admin.entity.AdminRole;
+
+public record AdminRoleUpdateRequest(AdminRole role) {
+}
---
+++ src/main/java/org/backend/domain/admin/dto/response/AdminRoleUpdateResponse.java
@@ -0,0 +1,4 @@
+package org.backend.domain.admin.dto.response;
+
+public record AdminRoleUpdateResponse() {
+}
