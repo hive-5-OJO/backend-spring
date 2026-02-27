de7960eb5d8659d87410b9caabcbeae2723b37ce
bakyunsu00 <zgustn0@gmail.com>
2026-02-27 16:49:02 +0900

    feat: 고객 필터링 reqeust dto 추가

---
+++ src/main/java/org/backend/domain/member/dto/CustomerFilterRequest.java
@@ -0,0 +1,14 @@
+package org.backend.domain.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerFilterRequest {
+    private String segment;
+    private String frequency;
+    private Long categoryId;
+}
